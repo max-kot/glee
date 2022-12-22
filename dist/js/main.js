8b1740a828f52bfa7402b69bb1fe6e27d022fbf3
sim-max <max.kotlarenko@gmail.com>
2022-12-22 12:23:56 +0300

    added section product-block

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,34 +1,39 @@
 import $ from "../../node_modules/jquery/dist/jquery.js";
 window.jQuery = $;
 
-// Range-slider
-import './modules/_range-slider.js';
-
-// Styler
-import './modules/_form-styler.js';
-
 // Slick slider
 import './modules/_slick.js';
-
-// Fancybox
-import './modules/_fancybox.js';
 
 // Burger-menu
 import './modules/_burger-menu.js';
 
-// Acordion 
-import './modules/_acordion.js';
+// Mixitup-menu
+import './modules/_mixitup.js';
+
+// Range-slider
+//import './modules/_range-slider.js';
+
+// Styler
+//import './modules/_form-styler.js';
+
+
+// Fancybox
+//import './modules/_fancybox.js';
+
+
+// Acordion
+//import './modules/_acordion.js';
 
 // Star Rating @Фрилансер по Жизни
-import './modules/_star-rating--byFreelancer.js'
+//import './modules/_star-rating--byFreelancer.js'
 
 // Таймер обратного отсчёта
-import './modules/_countdown.js';
+//import './modules/_countdown.js';
 
 // shop view mode
-import './modules/_shop-views.js';
+//import './modules/_shop-views.js';
 
 // filters btn
-import './modules/_filter-btn.js';
+//import './modules/_filter-btn.js';
 
-import './modules/_tabs.js';
+//import './modules/_tabs.js';
---
+++ src/js/modules/_mixitup.js
@@ -0,0 +1,3 @@
+import mixitup from "../../../node_modules/mixitup/dist/mixitup.js";
+
+var mixer = mixitup('.product-block__product-list');
